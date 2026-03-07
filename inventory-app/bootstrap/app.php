7748f8301c185b5fff0a0cbc2e60acdf99a2ed11
Lananuranf <lananuranf@gmail.com>
2026-03-07 22:41:37 +0700

    fix bootstrap middleware syntax

--- inventory-app/bootstrap/app.php
+++ inventory-app/bootstrap/app.php
@@ -13,10 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            ForceJsonResponse::class,
-        ]);
-
+        $middleware->statefulApi();
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
