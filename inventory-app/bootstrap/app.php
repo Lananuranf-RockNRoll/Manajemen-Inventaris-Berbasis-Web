5a2dccad77f89e0f14cd4671a08222d11175f3ee
Maulogika <lananuranf@gmail.com>
2026-03-08 12:16:47 +0700

    Update app.php

--- inventory-app/bootstrap/app.php
+++ inventory-app/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->prepend(\App\Http\Middleware\CorsMiddleware::class);
         $middleware->statefulApi();
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
