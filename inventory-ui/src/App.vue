e6515289c3b2e91745e2b2d482277b83e47ab142
Lananuranf <lananuranf@gmail.com>
2026-03-09 03:44:29 +0700

    Fix Login no use after 3 menit

--- inventory-ui/src/App.vue
+++ inventory-ui/src/App.vue
@@ -1,7 +1,39 @@
 <template>
-  <RouterView />
+  <RouterView/>
 </template>
 
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
+<script lang="ts" setup>
+import {onMounted, onUnmounted} from 'vue'
+import {useAuthStore} from '@/stores/auth'
+import {useRouter} from 'vue-router'
+
+const auth = useAuthStore()
+const router = useRouter()
+let idleTimer: ReturnType<typeof setTimeout>
+
+function resetTimer() {
+  clearTimeout(idleTimer)
+  idleTimer = setTimeout(() => {
+    if (auth.isAuthenticated) {
+      auth.logout()
+      router.push('/login')
+    }
+  }, 5 * 60 * 1000) // 3 menit
+}
+
+onMounted(() => {
+  window.addEventListener('mousemove', resetTimer)
+  window.addEventListener('keydown', resetTimer)
+  window.addEventListener('click', resetTimer)
+  window.addEventListener('scroll', resetTimer)
+  resetTimer()
+})
+
+onUnmounted(() => {
+  window.removeEventListener('mousemove', resetTimer)
+  window.removeEventListener('keydown', resetTimer)
+  window.removeEventListener('click', resetTimer)
+  window.removeEventListener('scroll', resetTimer)
+  clearTimeout(idleTimer)
+})
 </script>
--- inventory-ui/src/api/index.ts
+++ inventory-ui/src/api/index.ts
@@ -1,31 +1,31 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://manajemen-inventaris-berbasis-web-production.up.railway.app/api',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
+    baseURL: 'https://manajemen-inventaris-berbasis-web-production.up.railway.app/api',
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
 })
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token')
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
+    const token = localStorage.getItem('token')
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
 })
 
 api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token')
-      localStorage.removeItem('user')
-      window.location.href = '/login'
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem('token')
+            localStorage.removeItem('user')
+            window.location.href = '/login'
+        }
+        return Promise.reject(error)
     }
-    return Promise.reject(error)
-  }
 )
 
 export default api
